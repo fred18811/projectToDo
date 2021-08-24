501cb9df8437b1a0425cc2df1b60842cdf408dc5
Sergey <fred17@mail.ru>
2021-08-24 12:58:19 +0300

    dz2 - added component Users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import UserList from "./components/Users.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+        const users = [
+            {
+                "username": "User0",
+                "first_name": "Tester0",
+                "last_name": "Testerov0",
+                "email": "test0@test.ru"
+            },
+            {
+                "username": "User0",
+                "first_name": "Tester0",
+                "last_name": "Testerov0",
+                "email": "test0@test.ru"
+            },
+        ]
+        this.setState(
+            {'users': users}
+        )
+    }
+
+    render (){
+        return(
+            <UserList users={this.state.users}/>
+        )
+    }
+
+
 }
 
 export default App;
---
+++ frontend/src/components/Users.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>{user.username}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.email}</td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <tr>
+                <th>Login</th>
+                <th>Firest name</th>
+                <th>Last name</th>
+                <th>email</th>
+            </tr>
+            {users.map((user) => <UserItem user={user}/>)}
+        </table>
+    )
+}
+
+export default UserList
