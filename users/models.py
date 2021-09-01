c6c32d23921c4f38d2d9813a1fb15f36e7ced5dc
Sergey <fred17@mail.ru>
2021-09-01 15:07:17 +0300

    updated User in the models

--- users/models.py
+++ users/models.py
@@ -7,3 +7,7 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = "Пользователи"
