6641740e02735c2ebc464c09215442451f92ab94
Sergey <fred17@mail.ru>
2021-09-01 16:26:27 +0300

    added filter DateTimeFilter to  ToDoFilter

--- todo/filters.py
+++ todo/filters.py
@@ -5,10 +5,12 @@ from todo.models import ToDo, Project
 
 class ToDoFilter(filters.FilterSet):
     project = filters.CharFilter(field_name="project__name", lookup_expr='contains')
+    date_start = filters.DateTimeFilter(field_name="data_create", lookup_expr='gte')
+    date_end = filters.DateTimeFilter(field_name="data_create", lookup_expr='lte')
 
     class Meta:
         model = ToDo
-        fields = ['project']
+        fields = ['project', 'date_start', 'date_end']
 
 
 class ProjectFilter(filters.FilterSet):
